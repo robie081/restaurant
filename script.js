f20e7f95e2c0ba2fdc50f6d12a97acc4a8c439fb
robie081 <robert.lantus@icloud.com>
2023-11-20 15:00:23 +0200

    Casa Lupu Overlay

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+// Casa Lupu Overlay
 
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
+const navSlideOverlay = () => {
 
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            // console.log(index);
-            // console.log(index / 7);
-            if (link.style.animation)
-                link.style.animation = '';
-            else
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+    const myNav = document.getElementById("myNav");
+    const hamburger = document.querySelector('.hamburger');
+    const cross = document.querySelector('.cross');
+    const logo = document.querySelector('.logo');
+    const main = document.querySelector('main');
+    const desktop = document.querySelector('.desktop');
+
+    const closeOverlay = () => {
+        myNav.style.width = "0";
+        logo.style.opacity = 1;
+        main.style.opacity = 1;
+        desktop.style.opacity = 1;
+    }
+    
+    hamburger.addEventListener('click', () => {
+        myNav.style.width = "22rem";
+        logo.style.opacity = 0.5;
+        main.style.opacity = 0.5;
+        desktop.style.opacity = 0.5;
+
+        main.addEventListener('click', () => {
+            closeOverlay();
         });
-
-        // Burger Animation
-        burger.classList.toggle('change');
+    });
+    
+    cross.addEventListener('click', () => {
+        closeOverlay();
     });
 }
 
-navSlide();
+navSlideOverlay();
