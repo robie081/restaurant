a5e13384ba7cf3d55b510f5ec684dbdc2891ea42
robie081 <robert.lantus@icloud.com>
2023-11-22 10:49:00 +0200

    sticky navbar

--- script.js
+++ script.js
@@ -15,22 +15,71 @@ const navSlideOverlay = () => {
         logo.style.opacity = 1;
         main.style.opacity = 1;
         desktop.style.opacity = 1;
+        enableScroll();
     }
     
+    const disableScroll = () => {
+        // document.body.classList.add("stop-scrolling");
+        document.querySelector('.content').classList.add("stop-scrolling");
+    }
+
+    const enableScroll = () => {
+        // document.body.classList.remove("stop-scrolling");
+        document.querySelector('.content').classList.remove("stop-scrolling");
+    }
+
     hamburger.addEventListener('click', () => {
         myNav.style.width = "20rem";
         logo.style.opacity = 0.5;
         main.style.opacity = 0.5;
         desktop.style.opacity = 0.5;
 
+        disableScroll();
+
         main.addEventListener('click', () => {
             closeOverlay();
         });
+
+        // function disableScroll() {
+        //     document.body.classList.add("stop-scrolling");
+        // }
     });
     
     cross.addEventListener('click', () => {
         closeOverlay();
+        enableScroll();
+
+        // function enableScroll() {
+        //     document.body.classList.remove("stop-scrolling");
+        // }
     });
 }
 
 navSlideOverlay();
+
+
+// Create a Sticky Navbar
+
+const stickyNavbar = () => {
+
+window.onscroll = function() {
+    myFunction();
+};
+const navbar = document.getElementById('navbar');
+const sticky = navbar.offsetTop;
+console.log(sticky);
+
+function myFunction() {
+    
+    console.log(window.scrollY);
+
+    if (window.scrollY >= sticky) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+}
+
+}
+
+stickyNavbar();
